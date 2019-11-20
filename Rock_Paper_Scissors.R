0a1d0cd58d066aa97f58eb90e14e4d58af2cb553
StackOverthink <56105976+StackOverthink@users.noreply.github.com>
2019-11-20 16:57:40 +0100

    Create Rock_Paper_Scissors.R

---
+++ Rock_Paper_Scissors.R
@@ -0,0 +1,24 @@
+# Basic Rock Paper Scissors game.
+# Enter your choice as a string with the first letter capitalized.
+# Note that should you enter your choice written incorrectly or without the first letter capitalized
+# you will always lose. 
+RPS <- function(choice){
+  #sample Pc choice
+  NPC <- sample(c("Rock", "Paper", "Scissors"), size = 1)
+  # draw as unique event
+  if(choice == NPC){
+    list("Did I win?" = "Draw", "Opponent" = paste("Your opponent chose", NPC))
+    
+  }
+  # All combinations that result in a win
+  else if(choice == "Rock" & NPC == "Scissors" | choice == "Paper" & NPC == "Rock" | choice == "Scissors" &
+          NPC == "Paper"){
+    list("Did I win?" = "You won", "Opponent" = paste("Your opponent chose", NPC))
+  }
+  #remaining combinations 
+  else{
+    list("Did I win?" = "You lost", "Opponent" = paste("Your opponent chose", NPC))
+  }
+}
+RPS("Rock")
+
