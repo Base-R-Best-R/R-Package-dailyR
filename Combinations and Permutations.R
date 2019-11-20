ab5262d09e101f43f2d2641c553d6444521748a8
StackOverthink <56105976+StackOverthink@users.noreply.github.com>
2019-11-20 16:22:46 +0100

    Create Combinations and Permutations.R

---
+++ Combinations and Permutations.R
@@ -0,0 +1,23 @@
+# The following functions allow for quick permutation and combination calculations.#
+# The code was written by Gregor Steiner and myself.
+#  Binomial coefficient
+binc <- function(n, k){
+  return(factorial(n) / (factorial(n-k) * factorial(k)))
+}
+# comb / perm function
+# where rep = T represents wheater elements are replaced or not and perm = T displays that the permutation 
+# of importance.
+comb_solv <- function(n, k, rep = T, perm = T){
+  if(rep == T & perm == T){
+    return(n^k)
+  } 
+  else if(rep == F & perm == T){
+    return(factorial(n) / factorial(n - k))
+  }
+  else if(rep == T & perm == F){
+    return(binc((n + k - 1), k))
+  }
+  else{
+    return(binc(n, k))
+  }
+}
