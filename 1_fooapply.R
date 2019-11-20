c25c2265f7ed35f33305cdac0bdbc1aa105dc8af
StackOverthink <56105976+StackOverthink@users.noreply.github.com>
2019-11-20 17:17:33 +0100

    Create 1_fooapply.R

---
+++ 1_fooapply.R
@@ -0,0 +1,19 @@
+# fooapply allows one to apply different simple functions to a vector.
+# Functions to apply have to simple enough to only need the specification of a vector.
+# The desired functions have to be fed into the function as a string and typo will lead to an error.
+fooapply <- function(x, functions = c("mean", "median", "sd", "IQR", "quantile"), na.rm = T){
+  func <- functions
+  lis <- list()
+  for(i in 1:length(func)){
+    if(na.rm == T){
+      eval(parse(text = paste0("lis[[", i,"]]", "<-", func[i], "(x, na.rm = T)")))
+    } 
+    else{
+      eval(parse(text = paste0("lis[[", i,"]]", "<-", func[i], "(x)")))
+    }
+  }
+  names(lis) <- functions
+  return(lis)
+}
+fooapply(c(1,2,3,4))
+
