fbbae2ed4ab1e8954f25b24721f8378f9221ba2f
StackOverthink <56105976+StackOverthink@users.noreply.github.com>
2019-11-23 20:09:40 +0100

    Update Where_2_eat_WU.R

--- Where_2_eat_WU.R
+++ Where_2_eat_WU.R
@@ -1,7 +1,7 @@
 # Where 2 eat function 
 # Scrapes Lunch menus of 2 WU campus restaurants the Mensa and Library Cafe.
 # Running the function will install packages required and call then return the menus of the current day
-# note that weekends will yield a few empy strings and Null since there ar eno lunch menus on weekends
+# note that weekends will yield a few empy strings and Null since there are no lunch menus on weekends
 ########################################################################################################
 where.2.eat <- function(){
   #to make sure every needed package is installed and loaded
@@ -14,26 +14,26 @@ where.2.eat <- function(){
     }
   }
   get.package(c("dplyr", "rvest", "lubridate"))
-  #
+  # website containing lunch menu
   library <- html("https://www.library-cafe.at/tagesmenue-wien/")
-  #
+  # scrape text of specific html node
   library.text <- library %>%
     html_nodes("span") %>%
     html_text()
-  #
+  # filter for weekdays
   library.tag <- library.text[which(substr(library.text, 1, 6) %in% c("Montag", "Dienst", "Mittwo","Donner", "Freita"))]
-  #
+  # remove repeated information
   library.tag.ex <- library.tag[seq.int(1, length(library.tag), 3)]
   names(library.tag.ex) <- c("Montag", "Dienst", "Mittwo","Donner", "Freita")
-  #
+  # find index of current day
   l <- which(substring(weekdays(Sys.Date()), first =  1, last = 6) == names(library.tag.ex))
   ##Mensa##
   mensa <- html("http://www.wumensa.at/")
-  #
+  # filter for specific html node text
   mensa.text <- mensa %>%
     html_nodes(".text_umal") %>%
     html_text()
-  #
+  # remove information thats not of interest
   mensa.text.ex <- mensa.text[c(1, 2, 4, 5, 7, 8, 10, 11, 13, 14)]
   ##return##
   return(list("Mensa" = mensa.text.ex, "Library" = cat(library.tag.ex[l])))
