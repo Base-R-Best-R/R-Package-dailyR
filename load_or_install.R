8667354633ada285422179ebd01af6debd60f2c9
StackOverthink <56105976+StackOverthink@users.noreply.github.com>
2020-01-19 13:47:58 +0100

    Update load_or_install.R

--- load_or_install.R
+++ load_or_install.R
@@ -2,13 +2,21 @@
 # given a vector of packages (as strings), this function first checks if a package is installed
 # if it is then it is only called otherwise its installed and then called
 get.package <- function(packages){
-  for( i in 1:length(packages)){
-  if(!require(packages[i], character.only = T)){
-    install.packages(packages[i])
-    }
-    library(packages[i], character.only = T)
-  }
+ suppressWarnings({
+  lst <- list()
+  lst[[1]] <- ls(package:base)
+   for( i in seq.int(length(packages))){
+    if(!require(packages[i], character.only = T)){
+     install.packages(packages[i])
+     }
+     library(packages[i], character.only = T)
+     lst[[i + 1]] <- eval(parse(text = paste0("ls(package:", packages[i], ")")))
+   }
+   s <- unlist(lst)})
+   if(length(s[duplicated(s)]) > 0){
+    warning(c("The following functions exist in multiple packages: \n", 
+                   paste(" ",s[duplicated(s)], "\n")))
+   }
 }
 #
 get.package()
-
